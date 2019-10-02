aaed445796242c96e1512087384938945c17c82d
Karina2004liz <eng.karina.cabrera@gmail.com>
2019-10-02 11:30:10 -0500

    Updates

---
+++ precache-manifest.fa6d4b3b9a42541dc089a2fdec088f17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8821468b898b37460c197930266d70",
+    "url": "/GDL003-lab-notes/index.html"
+  },
+  {
+    "revision": "547fe1130e01f3f8b1a3",
+    "url": "/GDL003-lab-notes/static/css/main.e7db18db.chunk.css"
+  },
+  {
+    "revision": "eaa89e41411fe240b2eb",
+    "url": "/GDL003-lab-notes/static/js/2.b0acfbf9.chunk.js"
+  },
+  {
+    "revision": "547fe1130e01f3f8b1a3",
+    "url": "/GDL003-lab-notes/static/js/main.4cd6e7b6.chunk.js"
+  },
+  {
+    "revision": "dc0036d93d9e9fbd418f",
+    "url": "/GDL003-lab-notes/static/js/runtime~main.1ae9ac07.js"
+  }
+]);
